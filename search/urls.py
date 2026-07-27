2954f9854d93da58ec0c102256ce0a5e58fe92f4
maximillianroach <maxsquid567@gmail.com>
2026-07-27 12:50:06 -0400

    dropdown menu for styles added

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("page", views.page, name="page")
+    path("page", views.page, name="page"),
+    path("search", views.search, name="search"),
+    path("styles", views.styles, name="styles"),
+    path("genres", views.genres, name="genres"),
+    path("artists", views.artists, name="artists")
 ]
--- search/views.py
+++ search/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import requests
+
+FASTAPI_URL = "http://localhost:5001"
 
 # Create your views here.
 def page(request):
     return render(request, "search/page.html")
+
+def search(request):
+    # extract the data from query
+    query = request.GET.get("query", "")
+    style = request.GET.get("style")
+
+    # get the response from fastapi server
+    resp = requests.post(url=f"{FASTAPI_URL}/search", 
+                         json={"query": query, "style": style})
+
+    # convert response to json
+    resp = resp.json()
+
+    # return json to browser
+    return JsonResponse(resp)
+
+def styles(request):
+    resp = requests.get(url=f"{FASTAPI_URL}/styles")
+    return JsonResponse(resp.json(), safe=False)
+
+def genres(request):
+    resp = requests.get(url=f"{FASTAPI_URL}/genres")
+    return JsonResponse(resp.json(), safe=False)
+
+def artists(request):
+    resp =  requests.get(url=f"{FASTAPI_URL}/artists")
+    return JsonResponse(resp.json(), safe=False)
+
+
+    
 
 
